8e893ab5ac308ca4a0d595d3fd155b4b9fa02bc7
goldyr <enzoefb@hotmail.com>
2025-05-21 15:20:18 -0300

    new json tag output, cleaner code

--- functions.ts
+++ functions.ts
@@ -1,5 +1,13 @@
 import { list, link, img, p, bold, italics, code, h1, h2, h3, quote } from "./elements.ts"
 
+export type BlogData = {
+	title?: string,
+	description?: string,
+	topic?: string,
+	tags: Array<string> | undefined,
+	date?: string
+};
+
 export const replace_text_with_element = (text: string, regex: RegExp, replacing_fun: Function, replacing_param: string): string => {
 	const matches = [...text.matchAll(regex)].map(m => m[1]);
 	matches.forEach((match) => {
@@ -103,42 +111,74 @@ export const peas = (text: string): string => {
 	return text;
 }
 
-export const tags = (text: unknown): { text: string, tags: Array<{ title: string, content: string }> } => {
+export const tags = (text: string): { text: string, tags: Array<{ title: string, content: string }>, json: BlogData | undefined } => {
 	const info_tags: Array<{ title: string, content: string }> = []
+	const json_tags: BlogData = { title: undefined, description: undefined, topic: undefined, tags: undefined, date: undefined };
 
 	const raw_tags: Array<string> = []
-	if (typeof (text) === 'string') {
-		if (text.match("---") != null) {
-			let split_bars = text.split("---");
-			if (split_bars[1] != undefined && split_bars[2] != undefined) {
-				let split_each_tag = split_bars[1].trimStart().split("\n");
-				split_each_tag.forEach(tag => raw_tags.push(tag));
-				text = split_bars[2].trimStart();
-			}
+	if (text.match("---") !== null) {
+		//---
+		//foo: bar
+		//lorem: epsum
+		//---
+		//split_bars => [0]="---" [1]="foo: bar\n lorem: epsum" [2]="---"
+		let split_bars = text.split("---");
+		if (split_bars[1] !== undefined && split_bars[2] !== undefined) {
+			let split_each_tag = split_bars[1].trimStart().split("\n");
+			split_each_tag.forEach(tag => raw_tags.push(tag));
+			text = split_bars[2].trimStart();
 		}
 
 		raw_tags.forEach((tag: string) => {
-			let splitted = tag.split(":");
-			if (splitted[0] === "tags" && splitted[1] !== undefined) {
-				let minitags = splitted[1].split(",");
-				minitags.forEach((mt) => info_tags.push({
-					title: "tags",
-					content: mt.replace("[", "").replace("]", "").replaceAll(`"`, "").trim()
-				}));
-			}
-			else if (splitted[1] !== undefined && splitted[0] !== undefined) {
-				info_tags.push({
-					title: splitted[0],
-					content: splitted[1].trim()
+			//Tags look like foo: bar
+			//split[0]=foo split[1]=bar
+			const splitted = tag.split(":");
+			const title = splitted[0];
+			const content = splitted[1];
+			if (title === "tags" && content !== undefined) {
+				json_tags.tags = []
+				const minitags = content.split(",");
+				minitags.forEach((mt) => {
+					const clean_content = mt.replace("[", "").replace("]", "").replaceAll(`"`, "").trim();
+					info_tags.push({
+						title: "tags",
+						content: clean_content
+					})
+					if (json_tags.tags !== undefined) {
+						json_tags.tags.push(clean_content);
+					}
 				})
 			}
+			else if (content !== undefined && title !== undefined) {
+				info_tags.push({
+					title: title,
+					content: content.trim()
+				})
+				switch (title) {
+					case "title":
+						json_tags.title = content.trim();
+						break;
+					case "topic":
+						json_tags.topic = content.trim();
+						break;
+					case "description":
+						json_tags.description = content.trim();
+						break;
+					case "date":
+						json_tags.date = content.trim();
+						break;
+				}
+			}
 		})
-		return { text: text as string, tags: info_tags }
+
+		return { text: text as string, tags: info_tags, json: json_tags }
 	}
-	return { text: "", tags: [] }
+	else {
+		return { text: "", tags: [], json: undefined }
+	}
 }
 
-export const readFile = async (filepath: string): Promise<string | undefined> => {
+export const read_file = async (filepath: string): Promise<string | undefined> => {
 	try {
 		const file: Bun.BunFile = Bun.file(filepath);
 		if (file.type === "text/markdown") {
@@ -164,8 +204,8 @@ export const md_to_html = (text: string): string => {
 	text = text.replaceAll("\r\n", "\n");
 	text = text.replaceAll("\r", "\n");
 
-	const tags_text = tags(text);
-	text = tags_text.text;
+	//const tags_text = tags(text);
+	//text = tags_text.text;
 
 	const bold_regex = /\*\*(.+?)\*\*/g;
 	text = replace_text_with_element(text, bold_regex, bold, "**");
@@ -201,7 +241,7 @@ export const md_to_html = (text: string): string => {
 	return text;
 };
 
-export const cleanFilename = (full_path: string): string => {
+export const clean_filename = (full_path: string): string => {
 	const splitted = full_path.split("/");
 	let file_name = splitted[splitted.length - 1];
 	if (file_name !== undefined) {
@@ -217,16 +257,19 @@ export const helper = (b: boolean) => {
 	if (b === true) {
 		console.log("md2h HELP\n" +
 			"Check the README for conversion rules \n" +
-			"ARGUMENTS: \n" +
+			"ARGUMENTS: \n\n" +
 			"-o(outputs HTML to /output)\n" +
 			"-h(outputs help)\n" +
 			"-a(outputs arguments)\n" +
 			"-nh(outputs the conversions without html or head, this is what would go in the body)\n" +
+			"-j(outputs tags in json format to /output/html_data.json)\n\n" +
 			"EXAMPLES: \n" +
 			"outputs the markdown converted to html: \n" +
 			"bun run index.ts README.md \n" +
 			"outputs the markdown converted to html to a file in ./output: \n" +
 			"bun run index.ts README.md -o \n" +
+			"outputs the markdown converted to html to a file ./output/file.html and creates a json file formated with tags ./output/html_data.json\n" +
+			`bun run index.ts file.md -o -j \n` +
 			"outputs just the basic structure directly converted without header\n" +
 			"bun run index.ts README.md -nh \n" +
 			"outputs the html without header to a .html file in ./output\n" +
@@ -237,7 +280,46 @@ export const helper = (b: boolean) => {
 			"-o(outputs HTML to /output)\n" +
 			"-h(outputs help)\n" +
 			"-a(outputs arguments)\n" +
-			"-nh(outputs the conversions without html or head, this is what would go in the body)\n");
+			"-nh(outputs the conversions without html or head, this is what would go in the body)\n" +
+			"-j(outputs tags in json format to /output/html_data.json)\n");
+	}
+}
+
+
+
+// NOTE: should receive an array of json files to do this operation only once
+export const json_writer = async (new_json: Array<BlogData>, create_new?: boolean): Promise<boolean> => {
+	try {
+		const FILEPATH = `./output/html_data.json`;
+		const file: Bun.BunFile = Bun.file(FILEPATH);
+		if (!await file.exists() || create_new === true) {
+			// NOTE:file doesnt exists, use the new data and write it
+			let bar = (JSON.stringify(new_json))
+			await Bun.write(FILEPATH, bar);
+			return true;
+		}
+		else {
+			// NOTE:file exists, convert it to a object, push the new data, write it 
+			let raw = await file.text()
+			const json_current = JSON.parse(raw);
+			json_current.forEach((current: BlogData) => {
+				new_json.forEach((next: BlogData) => {
+					if (current.title === next.title) {
+						current = next;
+						next.title = undefined;
+					}
+				})
+			})
+			new_json.forEach(obj => {
+				if (obj.title !== undefined) json_current.push(obj);
+			})
+			await Bun.write(FILEPATH, JSON.stringify(json_current));
+			return true;
+		}
+	}
+	catch (err: unknown) {
+		console.log("Error reading or creating json", err);
+		return false;
 	}
 }
 export default {}
--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import Bun from "bun"
-import { tags, helper, readFile, cleanFilename, md_to_html } from "./functions.ts"
+import { tags, helper, read_file, clean_filename, md_to_html, json_writer } from "./functions.ts"
+import { type BlogData } from "./functions.ts"
 
 //to edit the header
-const wrapInsideBody = (body_text: string, tags: Array<{ title: string, content: string }>): string => {
+const setup_headers = (body_text: string, tags: Array<{ title: string, content: string }>, file_name: string): string => {
 	let meta_tags = "";
-
 	//Typical meta tags
 	meta_tags += `<meta charset="UTF-8">\n`;
 	meta_tags += `<meta name="viewport" content="width=device-width, initial-scale=1.0">\n`;
@@ -23,8 +23,7 @@ const wrapInsideBody = (body_text: string, tags: Array<{ title: string, content:
 	}
 
 	if (!meta_tags.includes(`<title>`)) {
-		const default_title = "DEFAULT TITLE";
-		meta_tags += ("<title>" + default_title + "</title>\n");
+		meta_tags += ("<title>" + file_name + "</title>\n");
 	}
 
 	return `<!DOCTYPE html>
@@ -38,16 +37,17 @@ ${body_text}
 };
 
 const md2h = async (): Promise<number> => {
-	type args = { help: boolean, output_html: boolean, arguments_help: boolean, no_html_no_head: boolean };
+	type ARGS = { help: boolean, output_html: boolean, arguments_help: boolean, no_html_no_head: boolean, output_json_data: boolean };
 	let my_args: string[] = Bun.argv.slice(2, Bun.argv.length);
-	const arg_flags: args = {
+	const arg_flags: ARGS = {
 		help: my_args.includes("-h") ? true : false,
 		output_html: my_args.includes("-o") ? true : false,
 		arguments_help: my_args.includes("-a") ? true : false,
 		no_html_no_head: my_args.includes("-nh") ? true : false,
+		output_json_data: my_args.includes("-j") ? true : false,
 	};
 
-	my_args = my_args.filter(arg => ["-h", "-o", "-a", "-nh"].includes(arg) ? false : true);
+	my_args = my_args.filter(arg => ["-h", "-o", "-a", "-nh", "-j"].includes(arg) ? false : true);
 
 	if (arg_flags.help === true) {
 		helper(true);
@@ -58,39 +58,52 @@ const md2h = async (): Promise<number> => {
 		return 0;
 	}
 
+	const json_to_write: Array<BlogData> = [];
 	for (let i = 0; i < my_args.length; i++) {
 		const curr_arg = my_args[i];
 		if (curr_arg !== undefined) {
-			let text: string | undefined = await readFile(curr_arg)
+			let text: string | undefined = await read_file(curr_arg)
+			const file_name = clean_filename(curr_arg);
 			if (text !== undefined) {
 				const tags_text = tags(text);
 				text = tags_text.text;
-				if (typeof (text) === "string") {
-					const body_text = md_to_html(text);
-					if (arg_flags.output_html === true) {
-						let file_name = cleanFilename(curr_arg);
-						let full_html = "";
-						if (arg_flags.no_html_no_head === false) {
-							full_html = wrapInsideBody(body_text, tags_text.tags);
-						}
-						else {
-							full_html = body_text;
-						}
-						await Bun.write(`./output/${file_name}.html`, full_html)
-							.then(() => console.log(`./output/${file_name}.html created`))
-							.catch(err => console.log(`error writing ./output/${file_name}.html`, err))
-
-					}
-					else if (arg_flags.no_html_no_head === true) {
-						console.log(md_to_html(text));
+				const body_text = md_to_html(text);
+				if (arg_flags.output_html === true) {
+					let html_to_write = "";
+					if (arg_flags.no_html_no_head === false) {
+						html_to_write = setup_headers(body_text, tags_text.tags, file_name);
 					}
 					else {
-						console.log(wrapInsideBody(body_text, tags_text.tags));
+						html_to_write = body_text;
+					}
+					await Bun.write(`./output/${file_name}.html`, html_to_write)
+						.then(() => console.log(`./output/${file_name}.html created`))
+						.catch(err => console.log(`error writing ./output/${file_name}.html`, err));
+
+				}
+				else if (arg_flags.no_html_no_head === true) {
+					console.log(body_text);
+				}
+				else {
+					console.log(setup_headers(body_text, tags_text.tags, file_name));
+				}
+				if (arg_flags.output_json_data === true) {
+					if (tags_text.json !== undefined) {
+						json_to_write.push(tags_text.json);
+					}
+					else {
+						json_to_write.push({ title: file_name, tags: [] })
 					}
 				}
-			};
+			}
 		}
-	}
+	};
+
+	if (arg_flags.output_json_data === true && json_to_write.length > 0) {
+		if (await json_writer(json_to_write)) {
+			console.log("created/updated html_data.json");
+		}
+	};
 	return 0;
 }
 
